8c5a8c5fc28c6af9600851395c158258d9534751
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-08-08 15:27:51 -0300

    added DbConnector with methods for database connection

--- src/db/DbConnector.java
+++ src/db/DbConnector.java
@@ -1,6 +1,53 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DbConnector {
-	
-	
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if(conn == null) {
+            try {
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/coursejdbc?verifyServerCertificate=false&useSSL=true", "root", "1234567");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return conn;
+    }
+    public static void closeConnection() {
+        if(conn != null) {
+            try {
+                conn.close();
+            } catch(SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
 }
--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module workshop_javafx_jdbc {
 	requires javafx.fxml;
 	requires javafx.base;
 	requires javafx.graphics;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml, javafx.base, javafx.controls;
 	opens gui to javafx.graphics, javafx.fxml, javafx.base, javafx.controls;
